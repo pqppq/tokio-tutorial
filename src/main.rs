eb0d42585fb14429496b61ffeedbf189c2f60758
pqppq <yutatsu0119@gmail.com>
2024-06-14 23:05:51 +0900

    feat: use Arc/Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 pub async fn main() {
     // bind listener
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    // pseudo database
+    let mut db: Db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // accept a new incoming connection
         let (socket, _) = listener.accept().await.unwrap();
+        // make a clone of Arc pointer
+        let db = db.clone();
         // spawn tasks for each inbound socket
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // pseudo database
-    let mut db = HashMap::new();
 
     let mut connection = Connection::new(socket);
 
@@ -29,12 +37,14 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
